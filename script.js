70274739a97789b60239cc14a04b2ed5663f17e8
Nathan Jaguenaud <nathan@kimsufi3.jaguenaud.org>
2021-11-28 13:43:53 +0100

    Change rest server

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const userAction = async () => {
-  const response = await fetch('http://127.0.0.1:8000/portefolio/luis/get_poemes/');
+  const response = await fetch('http://kimsufi2.jaguenaud.org:25146/portefolio/luis/get_poemes/');
   const myJson = await response.json(); //extract JSON from the http response
   // do something with myJson
   console.log(myJson["poemes"]["snake"]["challenger"]);
